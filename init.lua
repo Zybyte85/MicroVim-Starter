ad78a972076d9d4449dd3e33f4a66ac4afc63177
Zybyte85 <cade.step01@gmail.com>
2025-03-15 08:13:00 -0600

    Added files

---
+++ init.lua
@@ -0,0 +1,5 @@
+require("config.lazy")
+require("mvim.config.options")
+require("mvim.config.mappings")
+
+require("config.options")
---
+++ lua/plugins/init.lua
@@ -0,0 +1,4 @@
+-- This is your plugins folder. Add plugins as documented here: https://lazy.folke.io/usage/structuring
+-- Default plugins are here: https://github.com/Zybyte85/MicroVim/tree/main/lua/mvim/plugins
+
+return {}
